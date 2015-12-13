37f04f7ea23743f385bc70f123c446375b39c289
ACGunion <kung.blog@gmail.com>
2015-12-13 20:31:47 +0800

    首次发布
    首次发布

--- nginx.sh
+++ nginx.sh
@@ -1,11 +1,20 @@
-cd $OPENSHIFT_DATA_DIR
-wget http://www.canonware.com/download/jemalloc/jemalloc-4.0.4.tar.bz2
-tar -xjf jemalloc-4.0.4.tar.bz2
-rm -rf jemalloc-4.0.4.tar.bz2
-wget ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.37.tar.gz
-tar -zxvf pcre-8.37.tar.gz
-rm -rf pcre-8.37.tar.gz
-wget https://github.com/FRiCKLE/ngx_cache_purge/archive/master.zip
-unzip master.zip
-rm -rf master.zip
-git clone git://github.com/alibaba/tengine.git
+#!/bin/bash
+
+# 下载环境编译程序
+echo "正在下载环境安装文件"
+cd ${OPENSHIFT_HOMEDIR}/app-root/repo
+git clone https://github.com/ACGunion/openshift-php7.git php7
+cp -r ${OPENSHIFT_HOMEDIR}/app-root/repo/php7/.openshift ${OPENSHIFT_HOMEDIR}/app-root/repo/php7/www ${OPENSHIFT_HOMEDIR}/app-root/repo/www
+rm -r -f ${OPENSHIFT_HOMEDIR}/app-root/repo/php7
+
+# 编译环境
+echo "开始安装环境"
+cd ${OPENSHIFT_HOMEDIR}/app-root/repo/.openshift/action_hooks
+chmod 6755 ${OPENSHIFT_HOMEDIR}/app-root/repo/.openshift/action_hooks/*
+source ${OPENSHIFT_HOMEDIR}/app-root/repo/.openshift/action_hooks/build
+source ${OPENSHIFT_HOMEDIR}/app-root/repo/.openshift/action_hooks/deploy
+
+# 开始运行
+echo "安装完成，开始启动环境程序"
+cd ${OPENSHIFT_HOMEDIR}/app-root/repo/.openshift/action_hooks
+source ${OPENSHIFT_HOMEDIR}/app-root/repo/.openshift/action_hooks/start
